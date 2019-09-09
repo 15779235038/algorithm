dd211b139d271eadd355e637099eeda7cbdef099
bao <530420396@qq.com>
2019-09-10 01:01:12 +0800

    860

--- all_algorithm/greedy/algorithm860/860best.py
+++ all_algorithm/greedy/algorithm860/860best.py
@@ -16,10 +16,31 @@
 
 #Reference:**********************************************
 
+import  copy
 class Solution:
     def lemonadeChange(self, bills):
-        # bills = sorted(bills)
-        pass
+
+
+        #仅仅用一个小变量，就可以轻易的将我们的10，5块钱记住，如果有的话，就-1，不然就加。聪明
+
+        five = ten = 0
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                if not five: return False
+                five -= 1
+                ten += 1
+            else:
+                if ten and five:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
+
 
 test = Solution()
-test.lemonadeChange([5,10,5,20])
+print(test.lemonadeChange([5,5,10,10,20]))
